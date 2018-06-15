7ec428208f756f8f8f6942ec56c0caf57e3c7bc7
LaoHu97 <962271461@qq.com>
2018-06-15 16:17:48 +0800

    :sparkles:version：1.0.0

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import wepy from 'wepy'
-let preUrlPath = 'https://dev.weupay.com'
-
-export const menu = params => { return wepy.request({url: 'https://dev.weupay.com/pay/syscore/menu', data: params, method: 'POST'}).then(res => res) }
+const extConfig = wepy.getExtConfigSync ? wepy.getExtConfigSync() : {}
+let preUrlPath = extConfig.attr.host
 
 export const selectOne = params => { return wepy.request({url: `${preUrlPath}/admin/api/wd/200/1/selectOne`, data: params, method: 'POST'}).then(res => res.data) }
 
