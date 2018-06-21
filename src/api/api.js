9557e82ae9cebb3a1feac53368c2f20f1f9a24c7
LaoHu97 <962271461@qq.com>
2018-06-21 09:47:53 +0800

    2018-6-21:更改api地址

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,10 @@ import wepy from 'wepy'
 const extConfig = wepy.getExtConfigSync ? wepy.getExtConfigSync() : {}
 let preUrlPath = extConfig.attr.host
 
-export const selectOne = params => { return wepy.request({url: `${preUrlPath}/admin/api/wd/200/1/selectOne`, data: params, method: 'POST'}).then(res => res.data) }
+export const selectOne = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/selectOne`, data: params, method: 'POST'}).then(res => res.data) }
 
-export const querySmallApp = params => { return wepy.request({url: `${preUrlPath}/admin/api/wd/300/1/querySmallApp`, data: params, method: 'POST'}).then(res => res.data) }
+export const querySmallApp = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/querySmallApp`, data: params, method: 'POST'}).then(res => res.data) }
 
-export const queryAppNews = params => { return wepy.request({url: `${preUrlPath}/admin/api/wd/400/1/queryAppNews`, data: params, method: 'POST'}).then(res => res.data) }
+export const queryAppNews = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/queryAppNews`, data: params, method: 'POST'}).then(res => res.data) }
 
-export const getNewDetail = params => { return wepy.request({url: `${preUrlPath}/admin/api/wd/400/1/getNewDetail`, data: params, method: 'POST'}).then(res => res.data) }
+export const getNewDetail = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/getNewDetail`, data: params, method: 'POST'}).then(res => res.data) }
