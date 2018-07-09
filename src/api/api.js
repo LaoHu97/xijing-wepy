dca12a5839db3f91339e891689540192acdc71c3
LaoHu97 <962271461@qq.com>
2018-07-09 09:20:05 +0800

    2018-7-9：新增校园导航

--- src/api/api.js
+++ src/api/api.js
@@ -10,3 +10,5 @@ export const querySmallApp = params => { return wepy.request({url: `${preUrlPath
 export const queryAppNews = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/queryAppNews`, data: params, method: 'POST'}).then(res => res.data) }
 
 export const getNewDetail = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/getNewDetail`, data: params, method: 'POST'}).then(res => res.data) }
+
+export const queryMiniNavig = params => { return wepy.request({url: `${preUrlPath}/admin/api/mini/1/queryMiniNavig`, data: params, method: 'POST'}).then(res => res.data) }
